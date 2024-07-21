31496667be0d1d20bbb54e62aa930e328e7b2846
AaronConstant <aconstant@pursuit.org>
2024-07-21 14:46:23 -0400

    moderate css display settings

--- src/Components/Herb.jsx
+++ src/Components/Herb.jsx
@@ -10,7 +10,7 @@ const Herb = ({ herb, id }) => {
             <Link to={`/herbs/${id}`}>{herb.name}</Link>
 
             </p>
-            <img src={herb.pic} alt="Herb img" />
+            <img className='herb__img' src={herb.pic} alt="Herb img" />
             <div className="herb__info-display">
                 <p>
                 { herb.astrology_sign }    
