4849e26b653b2a95589859b1a4381979fb08d6cf
Markedia Hinds <mhinds@pursuit.org>
2024-07-23 09:50:52 -0400

    Updates

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -1,16 +1,21 @@
+import { useState, useEffect } from 'react'
 import Herbs from '../Components/Herbs'
 import { IoFilter } from 'react-icons/io5'
 import { IoIosArrowDown } from 'react-icons/io'
+import { TfiLayoutGrid4Alt } from 'react-icons/tfi'
+import { TfiLayoutListThumb } from 'react-icons/tfi'
 import '../Styles/index.scss'
 
-const Index = ({ herbdata }) => {
-    
+const API = import.meta.env.VITE_API_URL
+
+const Index = ({herbdata}) => {
     return (
         <div className='index'>
-            <h2 className='index__catalog'>Catalog</h2>
+
+            <h2 className='index__title'>Catalog</h2>
             <button className='index__filter'>FILTER</button>
             <button className='index__sort'>SORT BY</button>
-            <h5 className='index__total'>{`${ herbdata ? herbdata.length : NaN } products`}</h5>
+            <h5 className='index__total'>{`${3} products`}</h5>
 
             <Herbs className='index__products'/>
         </div>
